45f3250f161a2d8ddde818a50f1ab3b545d767e3
Frank Zhao <fzhao@iix.net>
2015-12-14 15:20:29 +1000

    Self determine IP address and sample BIRD config

--- Makefile
+++ Makefile
@@ -10,7 +10,7 @@ build:
 	@docker build -t $(TAG) $(ROOT)
 
 run:
-	@docker run -it -d -v `pwd`/routes:/root/routes --name $(NAME) $(TAG)
+	@docker run -it -d --privileged -v `pwd`/routes:/root/routes -v `pwd`/startbgp.sh:/bin/startbgp --name $(NAME) $(TAG)
 
 clean:
 	@docker stop $(NAME)
--- startbgp.sh
+++ startbgp.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./bgp_simple.pl -myas 65000 -myip 172.17.0.11 -peerip 172.17.0.21 -peeras 65000 -p /root/routes -holdtime 6000 -m 2000 -n
+./bgp_simple.pl -myas 65000 -myip `/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1 }'` -peerip 172.17.0.3 -peeras 65000 -p /root/routes -holdtime 6000 -m 2000 -n
